4973be0a5638c50a1fa018cefd298479bc0c972b
AjmalRasouli <ajrasouli@hotmail.com>
2026-06-28 18:44:24 +0100

    Import Azure Network Security Group into Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,39 @@ resource "azurerm_resource_group" "rg" {
     Owner       = "Ajmal"
   }
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-enterprise-lab"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    Environment = "Lab"
+    Project     = "Enterprise-M365-Azure-Lab"
+    Owner       = "Ajmal"
+  }
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = [
+    "10.0.0.0/24"
+  ]
+}
+
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-enterprise-lab"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Environment = "Lab"
+    Project     = "Enterprise-M365-Azure-Lab"
+    Owner       = "Ajmal"
+  }
+}
